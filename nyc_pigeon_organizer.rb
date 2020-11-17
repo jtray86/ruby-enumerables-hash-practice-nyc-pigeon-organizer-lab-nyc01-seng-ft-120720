0e45999a79067d636705194a48a6244628a98859
Jen Tracy <jenniferetracy@gmail.com>
2020-11-17 15:53:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  
-  nyc_pigeon_organizer.uniq {|nyc_pigeon_organizer| new_pigon_organizer = hash.new}
+  new_pigon_organizer = hash.new
+  nyc_pigeon_organizer.uniq {|nyc_pigeon_organizer|new_pigon_organizer << }
   
 end
